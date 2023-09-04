de023b4b41e0bb1350dd53b5679e90f73a5838a9
kosta65434821 <99015300+kosta65434821@users.noreply.github.com>
2023-09-04 21:20:37 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
-alert(123);
-var WebApp = window.Telegram.WebApp;
-WebApp.showAlert(`Добро пожаловать, @${WebApp.WebAppUser.username}.`);
+document.addEventListener("DOMContentLoaded", function(){
+  alert(123);
+  var WebApp = window.Telegram.WebApp;
+  WebApp.showAlert(`Добро пожаловать, @${WebApp.WebAppUser.username}.`);
+});
