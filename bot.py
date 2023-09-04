460bcb493b86c89ee8b63cbae052d30b1f927934
kosta65434821 <kosta65434921@gmail.com>
2023-09-04 21:15:15 +0400

    edit webapp

--- bot.py
+++ bot.py
@@ -1,17 +1,28 @@
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMurkup
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
 from aiogram.dispatcher import FSMContext
-from aiogram.contrlib.fsm_storage.memory import MemoryStorage
+from aiogram.utils import executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 TOKEN = '6037548848:AAEZugrYbuNvvqViP6LwYq5fwb7-9F7lU4A'
 storage = MemoryStorage()
 
-bot = Bot(TOKEN)
+bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=storage)
 
-webapp = InlineKeyboardButton(url='https://github.com/kosta65434821/cam/webapp/index.html')
-kb = InlineKeyboardMurkup().row(webapp)
+webapp = InlineKeyboardButton('webapp', type='web_app', web_app=WebAppInfo(url='https://kosta65434821.github.io/cam/'))
+kb = InlineKeyboardMarkup().row(webapp)
 
-def start(msg: types.Message, state: FSMContext) -> None:
+async def start(msg: types.Message, state: FSMContext) -> None:
 	user_id = str(msg.from_user.id)
-	await bot.send_message(user_id, 'WebApp', webapp)
+	await bot.send_message(user_id, 'WebApp', reply_markup=kb)
+
+def register_handlers() -> None:
+	dp.register_message_handler(start, commands='start', state='*')
+
+register_handlers()
+
+async def on_startup(_):
+	print('bot get started')
+
+executor.start_polling(dp, on_startup=on_startup)
